7bf7ad495efb0b28940cf18379a066c5f1605109
Diac Paul <diac.paul@quadom.com>
2021-04-29 18:38:08 +0300

    Performance tests

---
+++ PerformanceTests/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microcharts;
+using SkiaSharp;
+
+namespace PerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var chart = new BarChart
+            {
+                Entries = new List<ChartEntry>()
+                {
+                    new ChartEntry(10)
+                    {
+                        ValueLabelColor = new SKColor(255,0,255,255),
+                        TextColor  = new SKColor(1,255,255,255),
+                        Color  = new SKColor(1,1,255,255),
+                        Label = "USerializer",
+                        ValueLabel = "11"
+                    },
+                    new ChartEntry(88)
+                    {
+                        ValueLabelColor = new SKColor(255,0,255,255),
+                        TextColor  = new SKColor(1,255,255,255),
+                        Color  = new SKColor(1,1,255,255),
+                        Label = "USerializer",
+                        ValueLabel = "123"
+                    },
+                },
+                LabelOrientation = Orientation.Horizontal,
+                ValueLabelOrientation = Orientation.Horizontal
+            };
+
+            chart.MaxValue = 100;
+            chart.IsAnimated = false;
+            //chart.BackgroundColor = new SKColor(255, 255, 255);
+
+            // chart.LabelTextSize = 10;
+
+            var info = new SKImageInfo(1024, 1024, SKColorType.Rgba8888);
+
+            using (var surface = SKSurface.Create(info))
+            {
+                SKCanvas canvas = surface.Canvas;
+                canvas.Clear(SKColors.White);
+
+                //canvas.DrawColor(new SKColor(0, 0, 0), SKBlendMode.Src);
+                chart.AnimationProgress = 1;
+                chart.DrawContent(canvas, 1024, 1024);
+
+                //canvas.Save();
+
+
+                var paint = new SKPaint
+                {
+                    IsAntialias = true,
+                    Color = new SKColor(0x2c, 0x3e, 0x50),
+                    StrokeCap = SKStrokeCap.Round
+                };
+
+
+                canvas.Flush();
+                canvas.Save();
+
+
+                var snapshot = surface.Snapshot();
+
+                var file = $"output.png";
+
+                using (var data = snapshot.Encode(SKEncodedImageFormat.Png, 80))
+                {
+                    using (var stream = File.OpenWrite($"output.png"))
+                    {
+                        data.SaveTo(stream);
+                    }
+                }
+
+                Console.WriteLine(file);
+            }
+
+        }
+    }
+}
