2476db6aa221eda8099ad17e83ec1929a5449098
Diac Paul <diac.paul@quadom.com>
2021-07-09 17:53:46 +0300

    Header data is compared byte by byte to prvents unnaligned memory access crashes on android

--- USerializer/FieldData.cs
+++ USerializer/FieldData.cs
@@ -185,16 +185,6 @@ namespace USerialization
 
             return headerData;
         }
-        private static bool UnsafeCompare(byte* p1, byte* p2, int l)
-        {
-            byte* x1 = p1, x2 = p2;
-            for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
-                if (*((long*)x1) != *((long*)x2)) return false;
-            if ((l & 4) != 0) { if (*((int*)x1) != *((int*)x2)) return false; x1 += 4; x2 += 4; }
-            if ((l & 2) != 0) { if (*((short*)x1) != *((short*)x2)) return false; x1 += 2; x2 += 2; }
-            if ((l & 1) != 0) if (*((byte*)x1) != *((byte*)x2)) return false;
-            return true;
-        }
 
         private bool SameHeader(byte[] arr, int offset, int len)
         {
@@ -204,7 +194,14 @@ namespace USerialization
                 {
                     fixed (byte* pinned = _headerData)
                     {
-                        return UnsafeCompare(pinned + 1, arrPtr + offset, len);
+                        var arrayA = (pinned + 1);
+                        var arrayB = (arrPtr + offset);
+
+                        for (var i = 0; i < len; i++)
+                            if (arrayA[i] != arrayB[i])
+                                return false;
+
+                        return true;
                     }
                 }
             }
