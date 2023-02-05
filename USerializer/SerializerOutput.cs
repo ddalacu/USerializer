be2d60e36d59ddaa56740c0e3d4bb58ecd19b4c8
Diac Paul <paul.diac@quadom.com>
2023-02-05 21:02:19 +0200

    dotnet upgrade for action

--- USerializer/SerializerOutput.cs
+++ USerializer/SerializerOutput.cs
@@ -5,7 +5,6 @@ using System;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
 using Unity.IL2CPP.CompilerServices;
 
 namespace USerialization
