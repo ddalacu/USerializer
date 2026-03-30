a882f541e3a57832deea5aa6d5c779a8fc219335
Diac Paul <>
2026-03-30 19:26:45 +0300

    forgtot this

--- USerializer/ClassSerializationHelper.cs
+++ USerializer/ClassSerializationHelper.cs
@@ -8,7 +8,7 @@ namespace USerialization
 
         private readonly Type _serializedType;
 
-        internal ClassSerializationHelper(DataSerializer dataSerializer, Type serializedType)
+        public ClassSerializationHelper(DataSerializer dataSerializer, Type serializedType)
         {
             _dataSerializer = dataSerializer;
             _serializedType = serializedType;
@@ -22,32 +22,15 @@ namespace USerialization
 
         public void SerializeObject(object item, SerializerOutput output, object context)
         {
-            if (item == null)
-                throw new ArgumentException(nameof(item));
-            if (output == null)
-                throw new ArgumentException(nameof(output));
-
             var itemType = item.GetType();
-
-            if (itemType.IsValueType == false)
+            
+            if (itemType != _serializedType)
             {
                 if (_serializedType.IsAssignableFrom(itemType) == false)
                     throw new ArgumentException($"{itemType} cannot be casted to {_serializedType}");
-                
-                _dataSerializer.Write(SpanUtils.GetByteSpan(ref item), output, context);
-                return;
             }
-
-            if (itemType != _serializedType)
-                throw new ArgumentException(
-                    $"Type of the object {itemType} is not the same as the serializer type {_serializedType}");
-
-            throw new NotImplementedException();
-            // var pinnable = Unsafe.As<object, PinnableObject>(ref item);
-            // fixed (byte* objectAddress = &pinnable.Pinnable)
-            // {
-            //     _dataSerializer.Write(objectAddress, output, context);
-            // }
+            
+            _dataSerializer.Write(SpanUtils.GetByteSpan(ref item), output, context);
         }
 
         public object DeserializeObject(SerializerInput input, object context)
@@ -67,22 +50,6 @@ namespace USerialization
                 throw new ArgumentException($"{itemType} cannot be casted to {_serializedType}");
             
             _dataSerializer.Read(SpanUtils.GetByteSpan(ref item), input, context);
-        }
-    }
-
-    public static class ClassSerializationHelperExtensions
-    {
-        public static bool TryGetClassHelper(this USerializer serializer, out ClassSerializationHelper dataSerializer,
-            Type type, bool initializeDataSerializer = true)
-        {
-            if (serializer.TryGetDataSerializer(type, out var data, initializeDataSerializer) == false)
-            {
-                dataSerializer = default;
-                return false;
-            }
-
-            dataSerializer = new ClassSerializationHelper(data, type);
-            return true;
         }
     }
 }
